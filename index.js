7e0216ad38a729cc3e9b5876690c31a7d8c32f03
Joaquin <joaquinpalacio2311@gmail.com>
2023-08-22 13:03:54 -0300

    codigo ordenado

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const form = document.getElementById("contactForm");
-  const responseDiv = document.getElementById("response");
+document.addEventListener('DOMContentLoaded', function () {
+  const form = document.getElementById('contactForm');
+  const responseDiv = document.getElementById('response');
   const btn = document.getElementById('button');
 
-  form.addEventListener("submit", function (e) {
+  form.addEventListener('submit', function (e) {
     e.preventDefault();
 
     const formData = new FormData(form);
-    const phoneInput = formData.get("number");
+    const phoneInput = formData.get('number');
 
     // Validación de número de teléfono (solo números y longitud de 10 dígitos)
     if (!/^\d{10}$/.test(phoneInput)) {
-      responseDiv.innerHTML = '<div class="alert alert-danger">Ingrese un número de teléfono válido a 10 dígitos</div>';
+      responseDiv.innerHTML =
+        '<div class="alert alert-danger">Ingrese un número de teléfono válido a 10 dígitos</div>';
       return;
     }
 
@@ -20,10 +21,12 @@ document.addEventListener("DOMContentLoaded", function () {
     const serviceID = 'default_service';
     const templateID = 'template_ayfr1cn';
 
-    emailjs.sendForm(serviceID, templateID, form)
+    emailjs
+      .sendForm(serviceID, templateID, form)
       .then(() => {
         btn.value = 'Enviar Email';
-        responseDiv.innerHTML = '<div class="alert alert-success">Mensaje enviado con éxito</div>';
+        responseDiv.innerHTML =
+          '<div class="alert alert-success">Mensaje enviado con éxito</div>';
         form.reset();
 
         // Mostrar el mensaje de éxito durante 5 segundos
@@ -33,7 +36,8 @@ document.addEventListener("DOMContentLoaded", function () {
       })
       .catch((err) => {
         btn.value = 'Enviar Email';
-        responseDiv.innerHTML = '<div class="alert alert-danger">Error al enviar el mensaje. Por favor, inténtelo de nuevo más tarde.</div>';
+        responseDiv.innerHTML =
+          '<div class="alert alert-danger">Error al enviar el mensaje. Por favor, inténtelo de nuevo más tarde.</div>';
       });
   });
 });

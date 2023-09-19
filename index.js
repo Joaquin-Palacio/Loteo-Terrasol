096274d92489edaf96d5d5e96e59f0047c7b4f2c
Joaquin <joaquinpalacio2311@gmail.com>
2023-09-19 19:30:21 -0300

    logica button y boostrap

--- index.js
+++ index.js
@@ -1,44 +1,50 @@
-/* Funcion para formulario */
-document.addEventListener('DOMContentLoaded', function () {
-  const form = document.getElementById('contactForm');
-  const responseDiv = document.getElementById('response');
-  const btn = document.getElementById('button');
+// Función que muestra u oculta el botón según el scroll
+function mostrarBoton() {
+  var boton = document.getElementById('boton-inicio'); // Obtiene el botón por su id
+  if (window.scrollY > 200) {
+    // Si el scroll es mayor que 100 píxeles
+    boton.style.display = 'block'; // Muestra el botón
+  } else {
+    // Si no
+    boton.style.display = 'none'; // Oculta el botón
+  }
+}
 
-  form.addEventListener('submit', function (e) {
-    e.preventDefault();
+// Función que hace scroll hasta el inicio de la página
+function irAlInicio() {
+  window.scrollTo(0, 0); // Mueve el scroll a la posición (0, 0)
+}
 
-    const formData = new FormData(form);
-    const phoneInput = formData.get('number');
+// Añade un evento de tipo "scroll" a la ventana que llama a la función mostrarBoton
+window.addEventListener('scroll', mostrarBoton);
 
-    // Validación de número de teléfono (solo números y longitud de 10 dígitos)
-    if (!/^\d{10}$/.test(phoneInput)) {
-      responseDiv.innerHTML =
-        '<div class="alert alert-danger">Ingrese un número de teléfono válido a 10 dígitos</div>';
-      return;
-    }
+// Añade un evento de tipo "click" al botón que llama a la función irAlInicio
+var boton = document.getElementById('boton-inicio'); // Obtiene el botón por su id
+boton.addEventListener('click', irAlInicio);
 
-    btn.value = 'Enviando...';
 
-    const serviceID = 'default_service';
-    const templateID = 'template_ayfr1cn';
 
-    emailjs
-      .sendForm(serviceID, templateID, form)
-      .then(() => {
-        btn.value = 'Enviar Email';
-        responseDiv.innerHTML =
-          '<div class="alert alert-success">Mensaje enviado con éxito</div>';
-        form.reset();
+/* ========== JS de BOOSTRAP ========== */
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+  'use strict';
 
-        // Mostrar el mensaje de éxito durante 5 segundos
-        setTimeout(() => {
-          responseDiv.innerHTML = ''; // Limpiar el mensaje
-        }, 5000);
-      })
-      .catch((err) => {
-        btn.value = 'Enviar Email';
-        responseDiv.innerHTML =
-          '<div class="alert alert-danger">Error al enviar el mensaje. Por favor, inténtelo de nuevo más tarde.</div>';
-      });
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation');
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach((form) => {
+    form.addEventListener(
+      'submit',
+      (event) => {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add('was-validated');
+      },
+      false
+    );
   });
-});
+})();

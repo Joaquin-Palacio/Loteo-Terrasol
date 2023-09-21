696c93abd70d20f41c755f3173bc90b895eaa07e
Joaquin <joaquinpalacio2311@gmail.com>
2023-09-21 14:26:01 -0300

    modal con imagenes

--- index.js
+++ index.js
@@ -1,34 +1,29 @@
 // Función que muestra u oculta el botón según el scroll
 function mostrarBoton() {
-  let boton = document.getElementById('boton-inicio'); // Obtiene el botón por su id
+  let boton = document.getElementById('boton-inicio');
   if (window.scrollY > 200) {
-    // Si el scroll es mayor que 100 píxeles
-    boton.style.display = 'block'; // Muestra el botón
+    boton.style.display = 'block';
   } else {
-    // Si no
-    boton.style.display = 'none'; // Oculta el botón
+    boton.style.display = 'none';
   }
 }
 // Función que hace scroll hasta el inicio de la página
 function irAlInicio() {
   window.scrollTo(0, 0); // Mueve el scroll a la posición (0, 0)
 }
-// Añade un evento de tipo "scroll" a la ventana que llama a la función mostrarBoton
+
 window.addEventListener('scroll', mostrarBoton);
 
 // Añade un evento de tipo "click" al botón que llama a la función irAlInicio
-let boton = document.getElementById('boton-inicio'); // Obtiene el botón por su id
+let boton = document.getElementById('boton-inicio');
 boton.addEventListener('click', irAlInicio);
 
 /* ========== JS de BOOSTRAP ========== */
-// Example starter JavaScript for disabling form submissions if there are invalid fields
 (() => {
   'use strict';
 
-  // Fetch all the forms we want to apply custom Bootstrap validation styles to
   const forms = document.querySelectorAll('.needs-validation');
 
-  // Loop over them and prevent submission
   Array.from(forms).forEach((form) => {
     form.addEventListener(
       'submit',
@@ -44,3 +39,15 @@ boton.addEventListener('click', irAlInicio);
     );
   });
 })();
+
+// Modal para imagenes
+document.addEventListener('click', function (e) {
+  if (e.target.classList.contains('gallery-item')) {
+    const src = e.target.getAttribute('src');
+    document.querySelector('.modal-img').src = src;
+    const myModal = new bootstrap.Modal(
+      document.getElementById('gallery-modal')
+    );
+    myModal.show();
+  }
+});
